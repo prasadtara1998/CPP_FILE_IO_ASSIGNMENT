1a892c37530acbbc6e9cb7ad2d214b5f61eec0a1
prasadtara1998 <98399075+prasadtara1998@users.noreply.github.com>
2022-03-20 21:09:31 +0530

    Add files via upload

---
+++ 5_student.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+string name,location,branch;
+int id;
+fstream myfile;
+myfile.open("stud.txt",ios::app|ios::out);
+int c=0;
+do{
+cout<<"enter 1 to enter student detail\n2.Search\n3.exit"<<endl;
+cin>>c;
+switch(c){
+
+case 1: cout<<"enter student name,id,branch,location"<<endl;
+	cin>>name>>id>>branch>>location;
+	myfile<<name<<" ";
+	myfile<<id<<" ";
+	myfile<<branch<<" ";
+	myfile<<location<<endl;	
+	break;
+case 2: myfile.close();
+	myfile.open("stud.txt",ios::in);
+	string i;
+	int flag=0;
+	string str;
+	cout<<"enter id of student"<<endl;
+	cin>>i;
+	int line_count=0;
+	int line=0;
+	while(getline(myfile,str)){ 
+	line_count+=1;
+	for(int j=0;j<str.length();j++){
+	if(str[j]==i[0]){
+	flag=1;
+	line=line_count;
+	break;
+			}
+		}
+	}
+	
+	try{
+	if(flag==0){
+	throw flag;
+	break;
+		
+		}
+	else{
+	string read;
+	myfile.close();
+	myfile.open("stud.txt",ios::in);
+	for(int j=1;j<=line;j++){
+	getline(myfile,read);
+		}
+	cout<<read<<endl;
+	break;}
+		}
+		
+	catch(int x){
+		cout<<"student id not found"<<endl;
+	
+			}
+	
+	
+
+
+}
+
+
+
+
+
+}while(c<3);
+myfile.close();
+
+
+
+
+
+return 0;
+}
